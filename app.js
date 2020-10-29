c363feff8b390d1914dc116fb2981367743837d4
stunnerhash <utyadav07860@gmail.com>
2020-10-30 03:24:40 +0530

    lodash added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const https =require ("https");
 const bodyparser = require("body-parser");
 const app= express();
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "This is the home page. ";
 const aboutContent = "This is the about page";
@@ -16,7 +17,7 @@ app.use(express.static("public"));
 app.use(bodyparser.urlencoded({extended :true}));
 
 app.get("/",function(req,res){
-  res.render("home",{homeStartingContent});
+  res.render("home",{posts});
 })
 app.get("/about",function(req,res){
   res.render("about",{aboutContent})
@@ -37,7 +38,21 @@ var report={
 posts.push(report);
 console.log(posts);
 res.redirect("/")
-})
+});
+
+app.get("/projects/:projectName",function(req,res){
+  const requestedTitle = _.lowerCase(req.params.projectName);
+
+  posts.forEach(function(post){
+    const storedTitle = _.lowerCase(post.title);
+    if(storedTitle === requestedTitle){
+    console.log("match found");
+    }else{
+      console.log("not a match");
+    }
+  })
+});
+
 
 app.listen(process.env.PORT||3000, function(){
   console.log("server started on port 3000");
