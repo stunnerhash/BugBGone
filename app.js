4e69969f87e6b3dc6bcea11da23649064c0dd3ee
stunnerhash <utyadav07860@gmail.com>
2020-11-11 00:44:21 +0530

    completely new user interface with side navigations, backend needs work

--- app.js
+++ app.js
@@ -1,48 +1,75 @@
 //jshint esversion:6
 
+// const _ = require("lodash");
+// const https =require ("https");
 const express=require ("express");
-const https =require ("https");
 const bodyparser = require("body-parser");
 const ejs = require("ejs");
-const _ = require("lodash");
 const app= express();
 const mongoose =require('mongoose');
 
 const aboutContent = "This is the about page";
 const contactContent = "This is the content page  ";
-var posts=[];
 
-mongoose.connect("mongodb://localhost:27017/projectDB",{ useNewUrlParser : true });
+app.set('view engine','ejs');
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({extended :true}));
 
-const projectSchema= new mongoose.Schema({
+mongoose.connect("mongodb://localhost:27017/projectDB",{useNewUrlParser:true, useUnifiedTopology : true });
+
+
+const projectSchema = {
   name : String,
   description : String,
   tickets :[{
     title : String,
     description : String
   }]
-});
-const Project =mongoose.model("Project",projectSchema);
+};
+const Project = mongoose.model("Project",projectSchema);
 
-const project = new Project ({
+const project1 = new Project ({
+  name: "My Resume",
+  description: "Static Resume Website"
+});
+const project2 =new Project({
   name:"BugBGone",
   description : "An easy way to connect to developer",
-  $push : {
-      ticket :{
-      title: "Improve",
-      description : "Make it Responsive",
-    }
-  }
+})
+const project3 = new Project({
+  name: "Algorithm Visualizer",
+  description : "A simple path-algorithm visualizer ,to better understand the path finding algorithms",
+})
+const defaultProjects=[project1,project2,project3];
+
+Project.deleteMany()
+Project.insertMany(defaultProjects,function(err){
+if(err){
+  console.log(err);
+}else{
+  console.log("successfully added default projects");
+};
 });
 
-project.save();
+const userSchema={
+  email:String,
+  password:String
+}
+const User = new mongoose.model("User",userSchema);
 
-app.set('view engine','ejs');
-app.use(express.static("public"));
-app.use(bodyparser.urlencoded({extended :true}));
+
 
 app.get("/",function(req,res){
-  res.render("home",{posts});
+  res.render("login");
+})
+app.get("/register",function(req,res){
+  res.render("register");
+})
+app.get("/tickets",function(req,res){
+  res.render("tickets");
+})
+app.get("/projects",function(req,res){
+  res.render("projects");
 })
 app.get("/about",function(req,res){
   res.render("about",{aboutContent})
@@ -50,10 +77,34 @@ app.get("/about",function(req,res){
 app.get("/contact",function(req,res){
   res.render("contact",{contactContent})
 })
-
-app.get("/compose",(req,res)=>{
+app.get("/compose",function(req,res){
   res.render("compose");
 })
+
+
+
+// app.get("/compose/:projectName",function(req,res) {
+//   const projectName = _.lowerCase(req.params.projectName);
+  
+//   // posts.forEach(function(post){
+//   //   const storedTitle = _.lowerCase(post.title);
+//   //   if(storedTitle === requestedTitle){
+//   //     res.render("",{})
+//   //   }
+//   // })
+// });
+
+
+// app.get("/projects/:projectName",function(req,res){
+//   const requestedTitle = _.lowerCase(req.params.projectName);
+
+//   // posts.forEach(function(post){
+//   //   const storedTitle = _.lowerCase(post.title);
+//   //   if(storedTitle === requestedTitle){
+//   //     res.render("post",{post})
+//   //   }
+//   // })
+// });
 
 app.post("/compose",function(req,res){
 var report={
@@ -61,30 +112,45 @@ var report={
   description : req.body.description,
   tickets : tickets=[]
 };
-posts.push(report);
+// posts.push(report);
 res.redirect("/")
 });
 
-app.get("/compose/:projectName",function(req,res) {
-  const projectName = _.lowerCase(req.params.projectName);
-  
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
-    if(storedTitle === requestedTitle){
-      res.render("",{})
-    }
+app.post("/register",function(req,res){  
+  const newUser = new User({
+    email    : req.body.username,
+    password : req.body.password
   })
+  newUser.save(function(err){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("projects");
+    }
+  });
 });
 
-app.get("/projects/:projectName",function(req,res){
-  const requestedTitle = _.lowerCase(req.params.projectName);
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
-    if(storedTitle === requestedTitle){
-      res.render("post",{post })
+app.post("/", function(req,res){
+  const username =req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email:username},function(err,foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        if(foundUser.password === password){
+          res.render("projects");
+        }else{
+          console.log("Wrong password");
+        }
+      }else{
+        console.log("Error");
+      }
     }
-  })
+  });
 });
+
 
 app.listen(process.env.PORT||3000, function(){
   console.log("server started on port 3000");
